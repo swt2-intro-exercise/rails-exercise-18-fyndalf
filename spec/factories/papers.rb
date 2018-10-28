25b26b71ad9166e0f0fb2c74d2d0b1599d45c9a4
Finn K <finn-klessascheck@o2mail.de>
2018-10-28 19:34:57 +0100

    Add author-paper association

--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -3,10 +3,11 @@ FactoryBot.define do
     title { "MyString" }
     venue { "MyString" }
     year { 1 }
+    authors {build_list :author, 1}
   end
 end
 
 #    title "COMPUTING MACHINERY AND INTELLIGENCE"
 #    venue "Mind 49: 433-460"
 #    year 1950
-#    authors {build_list :author, 1}
+#
