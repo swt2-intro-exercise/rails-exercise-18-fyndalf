e3aa758298d076494d24d20104e5ebad4873fe94
Finn K <finn-klessascheck@o2mail.de>
2018-10-28 16:06:05 +0100

    Add author detail page

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Show author page", type: :feature do
+
+  it "should display author details" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+  end
+
+end
